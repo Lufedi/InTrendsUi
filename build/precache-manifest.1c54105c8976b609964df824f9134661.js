edf8e328b34cb0cc6533c9eecd8216a232450d89
Felipe Diaz <pipexir@gmail.com>
2019-05-06 11:07:48 -0500

    init

---
+++ build/precache-manifest.1c54105c8976b609964df824f9134661.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a70599fd92fba795848",
+    "url": "/static/css/main.b8e45b32.chunk.css"
+  },
+  {
+    "revision": "1a70599fd92fba795848",
+    "url": "/static/js/main.b5e69f96.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2ec691e021e8c10ff382",
+    "url": "/static/js/2.ec8a2cae.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1a1b7114b72fbd5a0e8f3fe2c0c559be",
+    "url": "/index.html"
+  }
+];
